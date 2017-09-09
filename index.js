a1a8d2de96e8a91775144ae38970f1c367c87cf1
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-09-09 20:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function nowServing(katzDeliLine){
     return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(katzDeliLine){
+  var currentLine = "The line is currently: ";
+  if (katzDeliLine.length == 0){
+    return "The line is currently empty.";
+  }
+  else {
+    for(i=0; i<katzDeliLine.length; i++){
+      currentLine += toString(i+1) +". " + katzDeliLine[i] + ", ";
+    }
+    return currentLine;
+  }
+}
