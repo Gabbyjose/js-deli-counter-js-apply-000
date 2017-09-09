375599d9c30ddd31be1f355130db952b17583221
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-09-09 20:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return "Welcome, " + name +". You are number " + katzDeliLine.length + " in line.";
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    var current = katzDeliLine.unshift();
+    return "Now serving " + current;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
